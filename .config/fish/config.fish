8dd54036383509c4eb6666228308ff21e358e3de
emintavlayan <emintavlayan@gmail.com>
2026-03-01 18:51:46 +0000

    Another Commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -58,16 +58,3 @@ alias cla="clear; ls -lah"
 
 fish_vi_key_bindings
 
-
-# ============================================================
-# Yazi
-# ============================================================
-
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	command yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
