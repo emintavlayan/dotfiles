9ca21be36bb0b1fcaa6fee1fe7142adff1d1f661
Emin Tavlayan <emintavlayan@gmail.com>
2026-03-05 16:28:39 +0000

    mesela

--- dot.config.ish/fish/config.fish
+++ dot.config.ish/fish/config.fish
@@ -58,3 +58,30 @@ alias cla="clear; ls -lah"
 
 fish_vi_key_bindings
 
+
+# ============================================================
+# Run ssh agent when fish starts 
+# ============================================================
+
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) >/dev/null
+    ssh-add ~/.ssh/id_ed25519 2>/dev/null
+end
+
+
+# ============================================================
+# Abbreviations
+# ============================================================
+abbr -a gco git checkout
+abbr -a gst git status
+abbr -a gcm git commit -m
+abbr -a gpl git pull
+abbr -a gps git push
+
+
+
+
+
+
+
+
